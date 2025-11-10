1f6e8b938dca623ec24b1cd359b8714ec42a3cb4
ago106 <168357108+ago106@users.noreply.github.com>
2025-11-11 00:06:23 +0700

    Create Week.lua

---
+++ DataBase/Week.lua
@@ -0,0 +1,48 @@
+local WeekKeys = {
+    -- Example: [""] = "",
+    --Week
+    ["42977047a4e45bf7f72feaef11ca6742"] = "", -- 916661200501243915 / 25.5.2025
+    ["db16c0624a6fffdef3f67b34b622db0d"] = "", -- 560398773189345290 / 26.5.2025
+    ["c4b5aff3e0ce8ad32fc4d20e32b64222"] = "", -- 448550510350303245 / 27.5.2025
+    ["BXpvyFjvkjRCTHNTjWjQTwValeSqrtOt"] = "", -- 1116697000117797054 / 26.9.2025
+    ["NropbXOrflWysQMZFdtjnKlVZUjpdcQP"] = "", -- 592155291693350923 / 6.9.2025
+    ["OxxmgysCFDiJxTPnsQrLhITVTWIwfkTm"] = "", -- 464971713378123816 / 8.10.2025
+    ["OgqqejgDLSvrkEEZiWGKwYoQeHdVHliQ"] = "", -- 422045582723645440 / 17.10.2025
+    ["nkvrTaCdQRwttPrRJQiYYVAwvwHPbiCu"] = "", -- 616172537688358932 / 28.10.2025
+    ["1383e5dcdccdd03d2741a7b6852afeda"] = "", -- 452064295115554847 / 31.7.2025
+    ["739ef1967f9adb69de400d4270262b65"] = "", -- 824923566880325673 / 18.10.2025
+    ["0cea73bea6206b37485a3114e2992f46"] = "", -- 595950831832924160 / 29.5.2025
+    ["742e516000ba3a42e45f3316b5a178a5"] = "", -- 765340589738688523 / 17.5.2025
+    ["acc400411791da508d79fbe9d9431d26"] = "", -- 1331067626989813874 / 23.7.2026
+    ["7aab2c18ed8e54187553adb3fcd52558"] = "", -- 1160269291015385239 / 17.5.2025
+    ["2bfadf057564a9bfba6cdba349dda87d"] = "", -- 1117087613078220820 / 8.6.2025
+    ["b18dd1bf86fdeedaf6eeae35ff3dfd36"] = "", -- 1171590593785372702 / 7.7.2025
+    ["FAKtkZtiNiHIeDiszbAuUEFklrqJmncU"] = "", -- 1372075505137877012 / 14.8.2025
+    ["igWhgkYACpfQPPXRApalYQbxtmgABSlX"] = "", -- 988859058318639175 / 10.8.2025
+    ["ahNWTQQOCpnbqQZNwSEwDepyysEXogfU"] = "", -- 1276768378752733227 / 30.8.2025
+    ["57d07d61c41dea9c73cafb83498dbaa7"] = "", -- 
+    ["78368704846d72b9c730757447dfd4b6"] = "", -- 
+    ["0e4fe9284c6ee73a0b9b5430fdbf988b"] = "", -- 
+    ["74bc5fbdc94bb6aca5ed597da52bce87"] = "", -- 
+    ["nkiXKiyqOwPTOVujczGwBXNhJmbBnQJl"] = "", -- 
+    ["YuMabufWstwsuodHzMQHupkQfBpdFRiF"] = "", -- 
+    ["LgfBVwrmPljuSLWiLhkSSrYvGfqHzgZG"] = "", -- 
+    ["yCGcOKKASiGMDzlOVckmezOlIqUmtTtL"] = "", -- 
+    ["swSlnecYxiCUSzohHXcTCDUlmHWAiLFL"] = "", -- 
+    ["BHKUJrXGEbdmudOmQXCpRDcMzyxthIDL"] = "", -- 
+    ["CHzGPVOTLTKJgicdBXxSmwqIAoclRyJd"] = "", -- 1058379870721298602 / 7.11.2025
+    ["hnnyKWVPUFLQQVivmiFAdMCDkhVrzYRY"] = "0a7e1ec83534814d2f5c56d0d54279acc079e3bf1dfa7b8b3160513453c4898a", -- 894930344057389076 / 12.11.2025
+    ["WttunOlDdeJvrwNwAHyyzsDWHufIVIdp"] = "45f9314aa76e3cddece06b7c9a335c1467b105b721573c728130b157fb5aac98", -- 1345020701681389568 / 15.11.2025
+    ["nEQFtLnTYjwhAnHUEwlnaPPIIjatSPxP"] = "", -- 
+    ["wnKtFELQrcbXYNvhuDNYxAyKHbUZgspX"] = "", -- 
+    ["oMBmHlLEQvNxmcGKbffpcSWDccmPxJVQ"] = "", -- 
+    ["jQUgtJGeIHPpmctPwTftDuFgFelqBEJS"] = "", -- 
+    ["zpXPfCOViEcXqeGAMKbvmRDyrEhStZUG"] = "", -- 
+    ["JdlrjWzLUBxspKLAkmIduZkXsuTsZiky"] = "", -- 
+    ["mmYczyFBiNqdBZWPeAZLGVvtfDSLJeuc"] = "", -- 
+    --["wJlnhjLXnacmHbtRixflWOCfjNMYZXZV"] = "dd04119c-ff6d-11ef-a030-806e6f6e6963", -- 1362475795611648120 / 9.11.2025
+    ["nKjDZZuzLMvIBwhgoYeGYAIsEVgwcqiF"] = "801361ca5b08eb0d3e9fddb107968370fb1056a3be7841222b0fa940ac778c5074e5ae261df22f6a5e02f0804f2d0b8a", -- 1024023389276491856 / 17.11.2025
+    [""] = "", -- id / id / time
+}
+
+return WeekKeys;
