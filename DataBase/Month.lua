c2f96cd832a79b6c81f58370252046315c8af2e2
ago106 <168357108+ago106@users.noreply.github.com>
2025-11-11 00:07:38 +0700

    Create Month.lua

---
+++ DataBase/Month.lua
@@ -0,0 +1,30 @@
+local MonthKeys = {
+    -- Example: [""] = "",
+    -- Month
+    ["e3cf2ae07a0db628474c43f2409ca0e5"] = "", -- 1165711973741232139 / 22.6.2025
+    ["463503394c53470a4a06b0deb7eb1f54"] = "", -- 681894090689740812 / 31.7.2025
+    ["e6e2c3a3e27a4cc134b6e79652c256b3"] = "", -- 1317865690056626207 / 19.8.2025
+    ["bfd2f60628b7b9ed2e907e6ad2b2cccc"] = "", -- 947804713615757343 / 21.8.2025
+    ["166d95b89986f6f85f576e507b32ac37"] = "", -- 1124252914228150303 / 29.8.2025
+    ["dnGDTwRIikexYXOQNfCMQNbHITENYPiQ"] = "", -- id / time
+    ["yXAvtAcePrUnpYEcCtccYrBnRDYQLDWq"] = "", -- id / time
+    ["GcBRVZYhZTrfMaBwgRdJPYtHpAJGdUAe"] = "", -- id / time
+    ["rUkclEwtKDaUmskydhjBklLthDCnZSAv"] = "", -- id / time
+    ["yKwKcRGpyyRmiMiFQcqVdnjYCNkjkrzm"] = "", -- id / time
+    ["tAWaJRFblboFzecEVUGECYFifrzhuyLS"] = "", -- id / time
+    ["tWykvyMgZbsbRHNGcdFTlupgZeFmRxSn"] = "", -- id / time
+    ["gEhsPukWYpgryRCPoIhszSWVqqdzTmiK"] = "", -- id / time
+    ["TGBxzyHGdOtFlfTbStOMGEmAzbvOJydc"] = "", -- id / time
+    ["gOZOGdYFXMwqalYLHsHYHWsAemEsywTz"] = "", -- 
+    ["CSPtDOhAGjriAZFltfNgVenGXvWKHfjg"] = "", -- 
+    ["PDenCLxxqGNjCLCqUrmjunpOJWkiDrsy"] = "", -- 
+    ["FnoWRUDXqYowEcLkiONIVLkTjmaAmkFP"] = "", -- 
+    ["LPfHRcIsbrsyWEDXSwOljujpJIpuspeJ"] = "", -- 
+    ["FEqFIGKWjLVszjWPzbjgFeiNJgwLeADf"] = "", -- 
+    ["fPmhrTWDYBvvcEPfwMWczbJFkvRnbNeB"] = "", -- 
+    ["OmHEXsRPEpcgBtraJrmiQReMIkFtnmtm"] = "", -- 
+    ["DToUjlEmSkEudVhoFAUxAcUeGXpyhBAj"] = "", -- 
+    ["hZEdVQzjpyMObHQwdVGkKFzYqxIkyyVA"] = "", -- 
+}
+
+return MonthKeys;
