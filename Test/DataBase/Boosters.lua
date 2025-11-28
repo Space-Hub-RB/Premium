20f098b50e8f1bd0c3f1f6f7996525d3fd44b96a
ago106 <168357108+ago106@users.noreply.github.com>
2025-11-28 16:06:58 +0700

    Create Boosters.lua

---
+++ Test/DataBase/Boosters.lua
@@ -0,0 +1,7 @@
+local BoosterKeys = {
+    -- ["LicenseKey"] = "", -- time / id
+    ["36096fa621c7fe81ef44e55b58319089"] = "hwid", -- 719947927203872770 / period / дата окончания ключа (к примеру 1.6.2025)
+    ["4b0d1af58861ebcfe89aeecf8d0c8aeb"] = "", -- 
+}
+
+return BoosterKeys;
