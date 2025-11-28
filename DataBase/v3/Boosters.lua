e9a245cd2a878ad91b1545367f7b0e66375690c8
ago106 <168357108+ago106@users.noreply.github.com>
2025-11-28 21:12:58 +0700

    Update HWID data - 2025-11-28 21:12

--- DataBase/v3/Boosters.lua
+++ DataBase/v3/Boosters.lua
@@ -1,4 +1,4 @@
 local BoosterKeys = {
-    ["bvobxydIRNROVvkOhiJkiaPBcmXiPGWH"] = "wqfuigh812gf12", -- 702417939000721421 / 90d / 26.02.2026
+    ["bvobxydIRNROVvkOhiJkiaPBcmXiPGWH"] = "eaighewogh9ew8g", -- 702417939000721421 / 90d / 26.02.2026
 }
 return BoosterKeys;
