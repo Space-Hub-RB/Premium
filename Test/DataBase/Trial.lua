4f9c5abe4e182e7f9257f0244d172e32cc8f69f8
ago106 <168357108+ago106@users.noreply.github.com>
2025-11-28 16:10:38 +0700

    Create Trial.lua

---
+++ Test/DataBase/Trial.lua
@@ -0,0 +1,6 @@
+local TrialKeys = {
+    ["e3cf2ae07a0db628474c43f2409ca0e5"] = "", -- 1165711973741232139 / period / Дата окончания (день.месяц.год)
+    ["463503394c53470a4a06b0deb7eb1f54"] = "", -- 
+}
+
+return TrialKeys;
