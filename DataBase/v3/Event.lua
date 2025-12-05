3b433be0ccd6874171d4d6e9be36bea3ec63573a
ago106 <168357108+ago106@users.noreply.github.com>
2025-12-05 22:24:16 +0700

    Update HWID data - 2025-12-05 22:24

--- DataBase/v3/Event.lua
+++ DataBase/v3/Event.lua
@@ -1,4 +1,4 @@
 local EventKeys = {
- ["9iDItW5pluGMBRpSRmNocGW6iuQM1Eph"] = "9h4egwt9hy39h89hwget4fh9h9wgtes43f", -- 702417939000721421 / lifetime
+ ["9iDItW5pluGMBRpSRmNocGW6iuQM1Eph"] = "waaaaaaaaaaaa", -- 702417939000721421 / lifetime
 }
 return EventKeys;
